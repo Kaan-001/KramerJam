9775c6d9896025850b39e5cd90d84d518941925c
Emre Kadir Karagöz <63540157+R1cHero@users.noreply.github.com>
2025-05-31 00:36:15 +0300

    player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 MovementSpeed = new Vector2(20.0f, 20.0f); // 2D Movement speed to have independant axis speed
+    private new Rigidbody2D rigidbody2D; // Local rigidbody variable to hold a reference to the attached Rigidbody2D component
+    private Vector2 inputVector = new Vector2(0.0f, 0.0f);
+
+    void Awake()
+    {
+        // Setup Rigidbody for frictionless top down movement and dynamic collision
+        // If RequireComponent is used uncomment the GetComponent and comment the AddComponent out
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        
+        rigidbody2D.angularDrag = 0.0f;
+        rigidbody2D.gravityScale = 0.0f;
+    }
+
+    void Update()
+    {
+        inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Rigidbody2D affects physics so any ops on it should happen in FixedUpdate
+        // See why here: https://learn.unity.com/tutorial/update-and-fixedupdate#
+        rigidbody2D.MovePosition(rigidbody2D.position + (inputVector * MovementSpeed * Time.fixedDeltaTime));
+    }
+}
