a4ba918bb5bf1e93b7b65e3fc618e8fa288f2f05
Kaan Kaplan <74473384+Kaan-001@users.noreply.github.com>
2025-05-31 21:09:39 +0300

    box added
    added

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 public class Box : MonoBehaviour
 {
     public bool isDestroyed = false;
+    private int HealthCurrent=2;
 
     [Header("Can prefablarý (0. index: Can)")]
     public GameObject[] Prefabs;
-
+    public Sprite Broken;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Bullet") && !isDestroyed)
         {
-            isDestroyed = true;
+            HealthCurrent -= 1;
 
-            if (Random.value <= 0.5f && Prefabs.Length > 0)
+            if (HealthCurrent <= 0) 
             {
-                GameObject spawnedObj = Instantiate(Prefabs[0], transform.position, Quaternion.identity);
+                if (Random.value <= 0.5f && Prefabs.Length > 0)
+                {
+                    GameObject spawnedObj = Instantiate(Prefabs[0], transform.position, Quaternion.identity);
 
-                Vector2 randomOffset = new Vector2(Random.Range(-1f, 1f), Random.Range(1f, 2f));
-                Vector3 jumpTarget = transform.position + (Vector3)randomOffset;
+                    Vector2 randomOffset = new Vector2(Random.Range(-1f, 1f), Random.Range(1f, 2f));
+                    Vector3 jumpTarget = transform.position + (Vector3)randomOffset;
 
-                spawnedObj.transform.DOJump(jumpTarget, 1.5f, 1, 0.5f).SetEase(Ease.OutQuad);
+                    spawnedObj.transform.DOJump(jumpTarget, 1.5f, 1, 0.5f).SetEase(Ease.OutQuad);
+                }
+                isDestroyed = true;
+                Destroy(gameObject);
+            }
+            else 
+            {
+               
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Broken;
             }
 
-            Destroy(this.gameObject, 0.4f);
         }
     }
 }
