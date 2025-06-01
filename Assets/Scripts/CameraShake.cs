7e038a117693f1d2b78a42231a0be73a36434591
Kaan Kaplan <74473384+Kaan-001@users.noreply.github.com>
2025-06-01 10:07:27 +0300

    Key istrigger added and cameraShake

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -7,11 +7,29 @@ public class CameraShake : MonoBehaviour
 {
     public float duration = 0.1f;
     public float strength = 0.2f;
-    public int vibrato = 10;
+    public int vibrato = 10, vibratoMin = 1;
     public static CameraShake instance;
     private void Start()
     {
+        StartCoroutine(ShakeRoutine());
         instance = this;
+    }
+    public float shakeIntervalMin = 0.1f;  // Min bekleme süresi
+    public float shakeIntervalMax = 0.3f;  // Max bekleme süresi
+
+  
+   
+    IEnumerator ShakeRoutine()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(shakeIntervalMin, shakeIntervalMax);
+            yield return new WaitForSeconds(waitTime);
+
+            // Salla ama çok hafif
+            transform.DOComplete(); // önceki varsa iptal et
+            transform.DOShakePosition(duration, strength/2, vibratoMin, 90f, false, true);
+        }
     }
     public void Shake()
     {
--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -6,7 +6,7 @@ public class Key : MonoBehaviour
 {
     private bool isKeyEquiped = false;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && isKeyEquiped)
         {
